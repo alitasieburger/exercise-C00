76dfac85df4a43f2e94a2078917be74ec4bca5be
asieburg <asieburg@student.42sp.org.br>
2022-02-16 01:03:54 +0100

    correção no nome de função auxíliar

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,13 +6,13 @@
 /*   By: asieburg <asieburg@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 00:10:47 by asieburg          #+#    #+#             */
-/*   Updated: 2022/02/16 00:39:19 by asieburg         ###   ########.fr       */
+/*   Updated: 2022/02/16 01:02:11 by asieburg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_print_numbers(int num_left, int num_right)
+void	ft_print_numbers_2(int num_left, int num_right)
 {
 	char	digits_left[2];
 	char	digits_right[2];
@@ -42,7 +42,7 @@ void	ft_print_comb2(void)
 		num_right = num_left + 1;
 		while (num_right <= 99)
 		{
-			ft_print_numbers(num_left, num_right);
+			ft_print_numbers_2(num_left, num_right);
 			num_right++;
 		}
 		num_left++;
